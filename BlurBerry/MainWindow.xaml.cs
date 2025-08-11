a248fce7e8f19c032c0db952b9360515014e6930
newtype0096 <newtype0096@naver.com>
2025-08-12 00:19:44 +0900

    WinUI로 전환
    WPF-UI에서 WinUI로 전환.

--- BlurBerry/MainWindow.xaml.cs
+++ BlurBerry/MainWindow.xaml.cs
@@ -1,26 +1,91 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Wpf.Ui.Appearance;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using BlurBerry.Views.Pages;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Vpn;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace BlurBerry
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public partial class MainWindow
+    public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            ApplicationThemeManager.Apply(this);
+            SetWindowProperties();
+
+            navigationView.SelectedItem = home;
+        }
+
+        private void SetWindowProperties()
+        {
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(titleBar);
+            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Standard;
+        }
+
+        public void Navigate(Type pageType, object? targetPageArguments = null, NavigationTransitionInfo? navigationTransitionInfo = null)
+        {
+            rootFrame.Navigate(pageType, targetPageArguments, navigationTransitionInfo);
+        }
+
+        private void titleBar_PaneToggleRequested(TitleBar sender, object args)
+        {
+            navigationView.IsPaneOpen = !navigationView.IsPaneOpen;
+        }
+
+        private void navigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                if (rootFrame.CurrentSourcePageType != typeof(SettingsPage))
+                {
+                    Navigate(typeof(SettingsPage));
+                }
+            }
+            else
+            {
+                var selectedItem = args.SelectedItemContainer;
+                if (selectedItem == home)
+                {
+                    if (rootFrame.CurrentSourcePageType != typeof(HomePage))
+                    {
+                        Navigate(typeof(HomePage));
+                    }
+                }
+                else if (selectedItem == image)
+                {
+                    if (rootFrame.CurrentSourcePageType != typeof(ImagePage))
+                    {
+                        Navigate(typeof(ImagePage));
+                    }
+                }
+                else if (selectedItem == video)
+                {
+                    if (rootFrame.CurrentSourcePageType != typeof(VideoPage))
+                    {
+                        Navigate(typeof(VideoPage));
+                    }
+                }
+            }
         }
     }
 }
